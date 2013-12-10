8b878efde2aec20197e232679ef068f5f6349892
Ryan Priebe <ryan@wearefree.ca>
2013-12-09 17:05:28 -0700

    minor refactoring

--- library/application/base/loader.php
+++ library/application/base/loader.php
@@ -4,18 +4,14 @@
 	class Loader {
 		//problem is here
 		public function view($name, $vars = array()){
-			$file = APP_PATH .'/views/'.$name.'.php';
-
-			//initialize the view class here to do a bunch of stuff, including
-			//loading the header and footer files from the current theme directory
-			$view = new View($name);
-
-			//wrap in try/catch for new exception type InvalidView
 			try {
-				if(is_readable($file)){
-					$view->load($file, $vars);
+				$view = new View($name);
+				$view_path = sprintf(APP_PATH ."/views/%s.php");
+
+				if(is_readable($view_path)){
+					$view->load($view_path, $vars);
 				}else {
-					throw new InvalidFileException(sprintf("Could not read file <strong>%s</strong>", $file));
+					throw new InvalidFileException(sprintf("Could not read file <strong>%s</strong>", $view_path));
 				}
 			} catch(InvalidFileException $e){
 				echo $e->getMessage();
@@ -26,7 +22,7 @@
 			try {
 				if(false === is_null($name)){
 					$model = new Model($name);
-					$model_path = sprintf(BASE ."/app/models/%s.php", $name);
+					$model_path = sprintf(APP_PATH ."/models/%s.php", $name);
 					
 					if(is_readable($model_path)){
 						return $model->load();
@@ -45,10 +41,9 @@
 			try {
 				if(false === is_null($name)){
 					$helper = new Helper($name);
-					$helper_path = sprintf(BASE ."/app/helpers/%s.php", $name);
+					$helper_path = sprintf(APP_PATH ."/helpers/%s.php", $name);
 
 					if(is_readable($helper_path)){
-						//return require($helper_path);
 						return $helper->load();
 					}
 
